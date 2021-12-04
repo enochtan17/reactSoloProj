5fa09b3ff38c76242d38a07ad66cef6db5befc60
Enoch Tan <enochtan@umich.edu>
2021-12-04 13:22:41 -0500

    phase1 done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const apiRouter = require('./api')
+
+router.use('/api', apiRouter)
 
 router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken())
-  res.send('Hello World!')
+    res.cookie('XSRF-TOKEN', req.csrfToken())
+    res.send('Hello World!')
 })
 
 module.exports = router
+
+// V9leA0cp-sxtCsocC7TQraPyop8nMqq9kEmc
