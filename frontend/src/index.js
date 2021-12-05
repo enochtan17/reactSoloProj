9b0583e6cf3351d79efbd45f3239c883a4cb69b1
Enoch Tan <enochtan@umich.edu>
2021-12-05 02:54:22 -0500

    login component done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider as ReduxProvider } from 'react-redux'
+import './index.css'
+import App from './App'
+import configureStore from './store'
+import { restoreCSRF, csrfFetch } from './store/csrf'
+import * as sessionActions from './store/session'
 
-const store = configureStore();
+const store = configureStore()
 
 if (process.env.NODE_ENV !== 'production') {
-  restoreCSRF();
+  restoreCSRF()
 
-  window.csrfFetch = csrfFetch;
-  window.store = store;
+  window.csrfFetch = csrfFetch
+  window.store = store
+  window.sessionActions = sessionActions
 }
 
 function Root() {
